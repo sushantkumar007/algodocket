a51d20d68c5d7ab437870070c3cb1e795223da0e
Sushant Kumar <sushantkumar5135@gmail.com>
2025-05-19 11:46:18 +0530

    feat: add utility functions for Judge0 for submission batch and polling batch

---
+++ Backend/src/libs/judge0.lib.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+const getJudge0LanguageId = (language) => {
+  const languageMap = {
+    PYTHON: 71,
+    JAVA: 62,
+    JAVASCRIPT: 63,
+  };
+
+  return languageMap[language.toUpperCase()];
+};
+
+const submitBatch = async (submissions) => {
+  const url = `${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`;
+  const { data } = await axios.post(url, { submissions });
+
+  return data;
+};
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const pollBatchResults = async (tokens) => {
+  while (true) {
+    const url = `${process.env.JUDGE0_API_URL}/submissions/batch`;
+    const { data } = await axios.get(url, {
+      params: {
+        tokens: tokens.join(","),
+        base64_encoded: false,
+      },
+    });
+
+    const results = data.submissions;
+
+    const isAllDone = results.every(
+      (r) => r.status.id !== 1 && r.status.id !== 2,
+    );
+    
+    console.log(isAllDone);
+    if (isAllDone) return results;
+    await sleep(1000);
+  }
+};
+
+const getLanguageName = (language_id) => {
+  const LANGUAGE_NAMES = {
+    62: "Java",
+    63: "JavaScript",
+    71: "Python",
+  };
+
+  return LANGUAGE_NAMES[language_id] || "Unknown";
+};
+
+export { getJudge0LanguageId, submitBatch, pollBatchResults, getLanguageName };
