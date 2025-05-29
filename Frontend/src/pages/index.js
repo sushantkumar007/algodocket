60a7ac3579c0e959ea35c2ddd07962d599de34d7
Sushant Kumar <sushantkumar5135@gmail.com>
2025-05-29 19:13:46 +0530

    add page imports

---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,13 @@
+import AddProblem from "./AddProblem"
+import HomePage from "./HomePage"
+import LoginPage from "./LoginPage"
+import ProblemPage from "./ProblemPage"
+import SignUpPage from "./SignUpPage"
+
+export {
+    AddProblem,
+    HomePage,
+    LoginPage,
+    ProblemPage,
+    SignUpPage,
+}
