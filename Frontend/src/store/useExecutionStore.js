f5649ea00a1619c29cb50f31e40214d03282f0fd
Sushant Kumar <sushantkumar5135@gmail.com>
2025-05-29 18:50:28 +0530

    add useExecutionStore

---
+++ Frontend/src/store/useExecutionStore.js
@@ -0,0 +1,32 @@
+import {create} from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useExecutionStore = create((set)=>({
+    isExecuting:false,
+    submission:null,
+
+       submitCode:async ( source_code, language_id, stdin, expected_outputs, problemId)=>{
+        try {
+            set({isExecuting:true});
+            console.log("Submission:",JSON.stringify({
+                source_code,
+                language_id,
+                stdin,
+                expected_outputs,
+                problemId
+            }));
+            const res = await axiosInstance.post("/execute-code/submit" , { source_code, language_id, stdin, expected_outputs, problemId });
+
+            set({submission:res.data.data.submission});
+      
+            toast.success(res.data.message);
+        } catch (error) {
+            console.log("Error executing code",error);
+            toast.error("Error executing code");
+        }
+        finally{
+            set({isExecuting:false});
+        }
+    }
+}))
