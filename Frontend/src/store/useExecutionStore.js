3dd23062d2b4dd0f34b649b423f5bd3132c14d9f
Sushant Kumar <sushantkumar5135@gmail.com>
2025-06-07 16:04:54 +0530

    feat: add execute code feature

--- Frontend/src/store/useExecutionStore.js
+++ Frontend/src/store/useExecutionStore.js
@@ -5,25 +5,31 @@ import toast from "react-hot-toast";
 export const useExecutionStore = create((set)=>({
     isExecuting:false,
     submission:null,
-
-       submitCode:async ( source_code, language_id, stdin, expected_outputs, problemId)=>{
+    
+    submitCode: async ( source_code, language_id, stdin, expected_outputs, problemId) => {
         try {
             set({isExecuting:true});
-            console.log("Submission:",JSON.stringify({
-                source_code,
-                language_id,
-                stdin,
-                expected_outputs,
-                problemId
-            }));
             const res = await axiosInstance.post("/execute-code/submit" , { source_code, language_id, stdin, expected_outputs, problemId });
 
             set({submission:res.data.data.submission});
-      
             toast.success(res.data.message);
         } catch (error) {
-            console.log("Error executing code",error);
-            toast.error("Error executing code");
+            toast.error("Code execution failed");
+        }
+        finally{
+            set({isExecuting:false});
+        }
+    },
+    
+    executeCode: async ( source_code, language_id, stdin, expected_outputs, problemId) => {
+        try {
+            set({isExecuting:true});
+            const res = await axiosInstance.post("/execute-code/execute" , { source_code, language_id, stdin, expected_outputs, problemId });
+
+            set({submission:res.data.data.submission});
+            toast.success(res.data.message);
+        } catch (error) {
+            toast.error("Code execution failed");
         }
         finally{
             set({isExecuting:false});
