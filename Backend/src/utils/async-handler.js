f76e64a5473b88f085ef99a4433a028434c223d2
Sushant Kumar <sushantkumar5135@gmail.com>
2025-06-07 15:52:09 +0530

    refactor: update syntax

--- Backend/src/utils/async-handler.js
+++ Backend/src/utils/async-handler.js
@@ -2,9 +2,8 @@ import { ApiError } from "./api-error.js";
 
 const asyncHandler = function(requestHandler) {
   return function (req, res, next) {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
-      const error = new ApiError(500, "Internal Server Error", err);
-      res.status(500).json(error);
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+      res.status(500).json(new ApiError(500, "Internal Server Error", error));
     });
   };
 };
