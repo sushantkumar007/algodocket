09e8ebbd6a7f88b4625a11d2152e6a211170711a
Sushant Kumar <sushantkumar5135@gmail.com>
2025-06-07 16:10:57 +0530

    refactor: update home page

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,46 +1,29 @@
 import React, { useEffect } from "react";
-
+import Container from "../components/Container";
+import Features from "../components/Features";
+import Hero from "../components/Hero";
+import Counts from "../components/Counts";
+import Footer from "../components/Footer";
 import { useProblemStore } from "../store/useProblemStore";
-import { Loader } from "lucide-react";
-import ProblemTable from "../components/ProblemTable";
+import { useAuthStore } from "../store/useAuthStore";
 
-const HomePage = () => {
+function HomePage() {
   const { getAllProblems, problems, isProblemsLoading } = useProblemStore();
-
+  
   useEffect(() => {
-    getAllProblems();
-  }, [getAllProblems]);
-
-  if(isProblemsLoading){
-    return (
-      <div className="flex items-center justify-center h-screen">
-          <Loader className="size-10 animate-spin"/>
-      </div>
-    )
-  }
+      getAllProblems();
+    }, [getAllProblems]);
 
   return (
-    <div className="min-h-screen flex flex-col items-center mt-14 px-4">
-      <div className="absolute top-16 left-0 w-1/3 h-1/3 bg-primary opacity-30 blur-3xl rounded-md bottom-9"></div>
-      <h1 className="text-4xl font-extrabold z-10 text-center">
-        Welcome to <span className="text-primary">LeetLab</span>
-      </h1>
-
-      <p className="mt-4 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10">
-        A Platform Inspired by Leetcode which helps you to prepare for coding
-        interviews and helps you to improve your coding skills by solving coding
-        problems
-      </p>
-
-      {
-        problems.length > 0 ? <ProblemTable problems={problems}/> : (
-            <p className="mt-10 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10 border border-primary px-4 py-2 rounded-md border-dashed">
-          No problems found
-        </p>
-        )
-      }
-    </div>
+    <>
+    <Container>
+      <Hero />
+      <Features />
+      <Counts />
+        <Footer />
+    </Container>
+    </>
   );
-};
+}
 
 export default HomePage;
