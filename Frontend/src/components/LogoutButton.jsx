f4e30b6949b64ab1f283aa32c78ce16effdebe9e
Sushant Kumar <sushantkumar5135@gmail.com>
2025-06-07 17:04:32 +0530

    fix: small fix

--- Frontend/src/components/LogoutButton.jsx
+++ Frontend/src/components/LogoutButton.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import { useNavigate } from "react-router-dom";
 
 
 const LogoutButton = ({children})=>{
     const {logout} = useAuthStore()
-
+    const navigate = useNavigate()
     const onLogout = async()=>{
         await logout();
-        
+        navigate("/login")
     }
 
 
