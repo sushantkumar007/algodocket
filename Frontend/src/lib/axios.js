aaabb081686b3e017b872df4300d61ccb7644435
Sushant Kumar <sushantkumar5135@gmail.com>
2025-06-08 17:51:17 +0530

    fix; error fix

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "https://myback.algodocket.com/api/v1",
   // baseURL: "http://localhost:8080/api/v1",
   // baseURL: "https://myback.algodocket.com/api/v1",
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: 'http://206.189.138.142:8080/api/v1',
   withCredentials: true,
 });
 console.log(".env: ", import.meta.env.VITE_BASE_URL)
