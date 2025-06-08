ac9f1329d02826b604e96ff63bd3d80bef91026d
Sushant Kumar <sushantkumar5135@gmail.com>
2025-06-08 17:33:44 +0530

    fix: tem domain error fix

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
-
-
 export const axiosInstance = axios.create({
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "https://myback.algodocket.com/api/v1",
   // baseURL: "http://localhost:8080/api/v1",
-  baseURL: "https://myback.algodocket.com/api/v1",
+  // baseURL: "https://myback.algodocket.com/api/v1",
+  baseURL: import.meta.env.BASE_URL,
   withCredentials: true,
 });
+console.log(".env: ", import.meta.env.VITE_BASE_URL)
